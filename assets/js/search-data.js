3f4df600fb7bb3e5450b987ee693a9df18cc6987
divz-k <117076907+divz-k@users.noreply.github.com>
2026-03-03 21:33:02 +0000

    Deploying to gh-pages from @ divz-k/divz-k.github.io@f0885d463e0d4482fae8667ddcaff6f160f6eef2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-cafa6-challenge",
+          title: 'CAFA6 Challenge',
+          description: "Predicting a protein&#39;s function from its amino acid sequence",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
