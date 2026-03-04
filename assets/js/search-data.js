06ada5e41cac8070c86580bd50d58f276148fb95
divz-k <117076907+divz-k@users.noreply.github.com>
2026-03-04 20:00:07 +0000

    Deploying to gh-pages from @ divz-k/divz-k.github.io@2815db747297e6f470b96ae4f457a9895c51b0a2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,11 +398,6 @@ ninja.data = [{
           description: "Predicting a protein&#39;s function from its amino acid sequence",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
